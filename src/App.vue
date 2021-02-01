1db4c37b9c381e8e900716e070c324e35f368f04
seonpark <pksung1@gmail.com>
2021-02-01 18:03:20 +0900

    vee-validation 작업중

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <h3>vee-validator 테스트</h3>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <v-form>
+        <v-container>
+          <v-row>
+            <v-col>
+              <validation-observer>
+                <validation-provider>
+                  <v-text-field v-model="userId" label="ID"/>
+                </validation-provider>
+                <validation-provider>
+                  <v-text-field v-model="userPw" label="비밀번호"/>
+                </validation-provider>
+                <validation-provider>
+                  <v-text-field v-model="userPw2" label="비밀번호 재입력"/>
+                </validation-provider>
+                <validation-provider>
+                  <v-text-field v-model="userEmail" label="이메일"/>
+                </validation-provider>
+                <validation-provider>
+                  <v-text-field v-model="userPhone" label="핸드폰번호"/>
+                </validation-provider>
+                <v-btn color="primary" align="left">완료</v-btn>
+              </validation-observer>
+            </v-col>
+          </v-row>
+        </v-container>
+
+      </v-form>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ValidationProvider, ValidationObserver } from 'vee-validate';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ValidationProvider, ValidationObserver
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
